33257b40224b5de50803ded77fa8c75d9b3b2f3e
Yeadi24 <rafiulhaque672@gmail.com>
2025-10-05 23:10:31 +0600

    I have completed navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -7,6 +7,9 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export const Navbar = () => {
   return (
@@ -16,7 +19,7 @@ export const Navbar = () => {
         <div className="w-12 h-10">
           <Link href="/" className="flex">
             <img src="Next Portal Logo Icon 2.jpg" alt="logo" />
-            <h1 className="font-bold text-blue-700">Next Portal</h1>
+            <h1 className="font-bold">Next Portal</h1>
           </Link>
         </div>
         {/* menu-bar */}
@@ -26,30 +29,70 @@ export const Navbar = () => {
               <NavigationMenuLink className="hover:text-red-600" href="/news">
                 News
               </NavigationMenuLink>
-              <NavigationMenuTrigger className="hover:text-red-600">
-                Services
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink className="hover:text-red-600">
-                  Web Dev
-                </NavigationMenuLink>
-                <NavigationMenuLink className="hover:text-red-600">
-                  SEO
-                </NavigationMenuLink>
-                <NavigationMenuLink className="hover:text-red-600">
-                  Mobile Apps
-                </NavigationMenuLink>
-              </NavigationMenuContent>
-              <NavigationMenuLink className="hover:text-red-600" href="/news">
+              <NavigationMenuLink
+                className="hover:text-red-600"
+                href="/services"
+              >
+                <NavigationMenuTrigger className="hover:text-red-600">
+                  Services
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="shadow-md rounded-md px-5 py-4 space-y-2">
+                    <li>
+                      <NavigationMenuLink
+                        href="/services/web"
+                        className="hover:text-red-600"
+                      >
+                        Web Dev
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/services/seo"
+                        className="hover:text-red-600"
+                      >
+                        SEO
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/services/mobile"
+                        className="hover:text-red-600"
+                      >
+                        Mobile Apps
+                      </NavigationMenuLink>
+                    </li>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuLink>
+
+              <NavigationMenuLink className="hover:text-red-600" href="/about">
                 About
               </NavigationMenuLink>
 
-              <NavigationMenuLink className="hover:text-red-600" href="/news">
+              <NavigationMenuLink
+                className="hover:text-red-600"
+                href="/contact"
+              >
                 Contact
               </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
+        {/* button and dark mode */}
+        <div className="hidden lg:flex">
+          <div className="flex justify-center items-center">
+            <h1 className="mr-3 text-black">Dark Mode</h1>
+            <Switch className="mr-4" />
+            <Button variant="default">Login</Button>
+          </div>
+        </div>
+        {/* mobile hamburger menu */}
+        <div className="lg:hidden flex">
+          <Button variant="default">
+            <AiOutlineMenu />
+          </Button>
+        </div>
       </nav>
     </header>
   );
